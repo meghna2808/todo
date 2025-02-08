9b1530fdfb344714ee458782bbcf428e37724d92
meghnas2808 <meghnasingh2808@gmail.com>
2025-02-09 01:16:07 +0530

    completed the functionality of all items

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -3,26 +3,45 @@ import React, { useState } from 'react'
 const Todo = () => {
     // const[tasks,setTasks]=use
     const[newTask,setNewTask]=useState("");
-    const[task,setTask]=useState(["eat breakfast","brush your teeths","do assingment"]);
+    const[task,setTask]=useState([]);
     const handleInputChange=(event)=>{
         setNewTask(event.target.value);
     }
     const addTask=()=>{
-        // setTask(newTask);
+        if(newTask.trim()!=="")
+        {
+            setTask([...task,newTask]);
+            setNewTask("")
+         }
     }
+        
     const deleteTask=(index)=>{
+        const updatedTasks=task.filter((_,i)=>i!=index);
+        setTask(updatedTasks);
 
     }
     const moveUp=(index)=>{
+        if(index>0)
+        {
+            const updatedTasks=[...task];
+            [updatedTasks[index],updatedTasks[index-1]]=[updatedTasks[index-1],updatedTasks[index]];
+            setTask(updatedTasks);
+        }
         
     }
     const moveDown=(index)=>{
+        if(index<task.length-1)
+        {
+            const updatedTasks=[...task];
+            [updatedTasks[index],updatedTasks[index+1]]=[updatedTasks[index+1],updatedTasks[index]];
+            setTask(updatedTasks);
+        }
         
     }
   return (
     <div>
         <div className='to-do-list'>
-        <h1>To do</h1>
+        <h1>To-Do-List</h1>
         <input placeholder='enter the text' type='text' value={newTask} onChange={handleInputChange}></input>
         <button className='add-button' onClick={addTask}>Add</button>
         </div>
